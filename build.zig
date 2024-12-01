707d20fa71fe54f58144a2e8d8e2bb3887cbcc77
voidfill <71205200+voidfill@users.noreply.github.com>
2024-12-01 12:09:45 +0100

    init test gitcrypt

---
+++ build.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+
+    _ = target;
+    _ = optimize;
+}
---
+++ src/1/solution.zig
@@ -0,0 +1,13 @@
+pub fn one(input: []const u8) u8 {
+    _ = input;
+    return 1;
+}
+
+test "one" {}
+
+pub fn two(input: []const u8) u8 {
+    _ = input;
+    return 2;
+}
+
+test "two" {}
